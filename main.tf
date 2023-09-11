13483a76ae7ce9887f062104893567c677b1cf8a
TORICES <torices.rafael@gmail.com>
2023-09-11 19:12:41 +0200

    Pequeño cambio en comentarios

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "google_compute_network" "keep-bonus-vpc" {
 
 # Recurso de bucket de almacenamiento en Cloud Storage
 resource "google_storage_bucket" "keep-bonus-bucket" {
-  name          = "${var.bucket_name}-${random_string.random.result}" # Nombre del bucket
+  name          = "${var.bucket_name}-${random_string.random.result}" # Nombre del bucket incluyendo un string aleatorio con la función definida anteriormente
   location      = var.location                                        # Zona de ubicación del bucket
   force_destroy = true                                                # Esto permitirá que Terraform elimine el bucket si se elimina el recurso de Terraform
 }
